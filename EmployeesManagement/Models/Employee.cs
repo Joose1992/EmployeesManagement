388901b91c1b171393de13ced33b86ee92e363d2
Jose Montanez <josemontanez1992@gmail.com>
2024-10-18 17:28:01 -0500

    Added models and set up appsettings

---
+++ EmployeesManagement/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeesManagement.Models
+{
+    public class Employee : UserActivity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string Department { get; set; }
+
+    }
+}
---
+++ EmployeesManagement/Models/UserActivity.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeesManagement.Models
+{
+    public class UserActivity
+    {
+        public string CreatedById { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}

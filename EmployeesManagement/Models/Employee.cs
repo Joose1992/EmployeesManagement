b2670b434902eff9160d33bd0d507b57f6903d1e
Jose Montanez <josemontanez1992@gmail.com>
2024-10-18 17:42:38 -0500

    - Added Migrations - Added Controllers and views

--- EmployeesManagement/Models/Employee.cs
+++ EmployeesManagement/Models/Employee.cs
@@ -6,6 +6,7 @@
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
+        public string FullName => $"{FirstName} {MiddleName.Substring(0,1)}. {LastName}";
         public string PhoneNumber { get; set; }
         public string EmailAddress { get; set; }
         public string Department { get; set; }
